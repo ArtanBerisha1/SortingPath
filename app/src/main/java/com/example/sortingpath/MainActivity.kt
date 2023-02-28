3ea3e17c761167d043a5caafee7d190e3e1099ea
Artan Berisha <berishaartan52@hotmail.com>
2023-02-28 22:04:35 +0100

    completed canvas, added a button to clear drawings

--- app/src/main/java/com/example/sortingpath/MainActivity.kt
+++ app/src/main/java/com/example/sortingpath/MainActivity.kt
@@ -1,15 +1,26 @@
 package com.example.sortingpath
 
 import android.os.Bundle
+import android.view.MotionEvent
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.input.pointer.pointerInteropFilter
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.sortingpath.ui.theme.SortingPathTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,10 +34,94 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    Surface {
 
+    val lastTouchX = remember {
+        mutableStateOf(0f)
     }
+
+    val lastTouchY = remember {
+        mutableStateOf(0f)
+    }
+
+    val path = remember {
+        mutableStateOf<Path?>(Path())
+    }
+
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(2.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Surface(
+            modifier = Modifier
+                .weight(0.9f),
+            color = Color.Gray
+        ) {
+            Canvas(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .pointerInteropFilter { motionEvent ->
+                        when (motionEvent.action) {
+                            MotionEvent.ACTION_DOWN -> {
+                                path.value?.moveTo(motionEvent.x, motionEvent.y)
+
+                                lastTouchX.value = motionEvent.x
+                                lastTouchY.value = motionEvent.y
+                            }
+
+                            MotionEvent.ACTION_MOVE, MotionEvent.ACTION_UP -> {
+                                val historySize = motionEvent.historySize
+                                for (i in 0 until historySize) {
+                                    val historicalX = motionEvent.getHistoricalX(i)
+                                    val historicalY = motionEvent.getHistoricalY(i)
+
+                                    path.value?.lineTo(historicalX, historicalY)
+                                }
+
+                                path.value?.lineTo(motionEvent.x, motionEvent.y)
+                                lastTouchX.value = motionEvent.x
+                                lastTouchY.value = motionEvent.y
+                            }
+                        }
+                        lastTouchX.value = motionEvent.x
+                        lastTouchY.value = motionEvent.y
+
+                        val tempPath = path.value
+                        path.value = null
+                        path.value = tempPath
+
+                        true
+                    },
+                onDraw = {
+                    path.value?.let {
+                        drawPath(
+                            path = it,
+                            color = Color.Black,
+                            style = Stroke(
+                                width = 4.dp.toPx()
+                            )
+                        )
+                    }
+
+                }
+            )
+        }
+
+        Button(
+            onClick = {
+                path.value = null
+                path.value = Path()
+            }
+        ) {
+            Text(text = "Clear")
+        }
+    }
+
+
 }
