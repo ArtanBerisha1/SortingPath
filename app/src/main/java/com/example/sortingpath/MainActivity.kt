09a6a32488fef251a3f10a9749a963ae75c5a1e5
Artan Berisha <berishaartan52@hotmail.com>
2023-02-28 20:05:54 +0100

    Initial commit

---
+++ app/src/main/java/com/example/sortingpath/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.sortingpath
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.sortingpath.ui.theme.SortingPathTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SortingPathTheme {
+                DefaultPreview()
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Surface {
+
+    }
+}
